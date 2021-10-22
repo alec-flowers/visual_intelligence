a2698faa80bf094e3a42de1f095682a6e68f0d24
Nina-Mainusch <Nina.Mainusch@protonmail.com>
2021-10-22 11:37:58 +0200

    FEAT: pose estimation, plotting, data loading. TODO on pose estimation format return

---
+++ main.py
@@ -0,0 +1,20 @@
+from data import *
+from utils import *
+from plot import *
+from pose import *
+
+
+# Define parameters
+shuffle = True
+
+# Load the data
+dataset, dataloader = load_data(path=TRAINPATH, batch_size=32, shuffle=shuffle)
+
+# Visualize random data points with their labels
+#plot_data(dataloader, n_images=4)
+
+# Do the pose estimation
+mp_pose = mp.solutions.pose # available poses
+estimated_poses, annotated_images = estimate_poses(dataloader)
+
+dataloader
---
+++ plot.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_data(dataloader, n_images=4):
+    images, labels = next(iter(dataloader))
+    for i in range(n_images):
+        image = images[i].numpy().transpose((1, 2, 0))
+        plt.subplot(int(np.sqrt(n_images)), int(np.sqrt(n_images)), i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(image)
+        plt.xlabel(dataloader.dataset.classes[labels[i].item()])
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_image(image):
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(image)
+    plt.tight_layout()
+    plt.show()
+
+
