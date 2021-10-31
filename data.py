b84d346dd69799dd7db950defde74b62ed99397b
Alec Flowers <alec.flowers00@gmail.com>
2021-10-31 13:30:57 +0100

    feat: change data loader to have 3 labels for the poses

--- data.py
+++ data.py
@@ -1,17 +1,56 @@
 import torch
 from torchvision import datasets, transforms
+from torchvision.datasets import ImageFolder, DatasetFolder
+from torch.utils.data import Dataset, DataLoader
 from utils import *
+from PIL import Image
+
+from typing import Any, Callable, Optional, Tuple, List, Dict
+
+IMG_EXTENSIONS = (".jpg", ".jpeg", ".png", ".ppm", ".bmp", ".pgm", ".tif", ".tiff", ".webp")
 
 
-def load_data(path=TRAINPATH, resize=300, batch_size=32, shuffle=True):
+def pil_loader(path: str) -> Image.Image:
+    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, "rb") as f:
+        img = Image.open(f)
+        return img.convert("RGB")
+
+
+class ClassifyDataset(ImageFolder):
+    def __init__(
+            self,
+            root: str,
+            loader: Callable[[str], Any] = pil_loader,
+            transform: Optional[Callable] = None,
+            is_valid_file: Optional[Callable[[str], bool]] = None
+    ):
+
+        super(ImageFolder, self).__init__(
+            root,
+            loader,
+            IMG_EXTENSIONS if is_valid_file is None else None,
+            transform=transform)
+
+    def find_classes(self, directory: str) -> Tuple[List[str], Dict[str, int]]:
+        classes = sorted(entry.name for entry in os.scandir(directory) if entry.is_dir())
+        if not classes:
+            raise FileNotFoundError(f"Couldn't find any class folder in {directory}.")
+
+        class_to_idx = {cls_names: i % 3 for i, cls_names in enumerate(classes)}
+
+        return classes, class_to_idx
+
+
+def load_data(path=TRAINPATH, resize=300, batch_size=32, shuffle=True) -> Tuple[ClassifyDataset, DataLoader]:
     transform = (transforms.Compose([transforms.Resize(resize),
                                      transforms.CenterCrop(resize-1),
                                      #transforms.Grayscale(num_output_channels=3),
                                      transforms.ToTensor(),
                                      transforms.ConvertImageDtype(torch.uint8)]))
 
-    dataset = datasets.ImageFolder(path, transform=transform)
+    dataset = ClassifyDataset(path, transform=transform)
 
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
 
     return dataset, dataloader
