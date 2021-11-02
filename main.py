3cc7f17d7ec31f637a668e77a36a1c7983cc3dee
Nina-Mainusch <Nina.Mainusch@protonmail.com>
2021-11-02 12:41:08 +0100

    REFACTOR: main

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from utils import *
 from plot import *
 from pose import *
 
-# Define parameters
-shuffle = True
+if __name__ == "__main__":
+    # Define parameters
+    shuffle = True
 
-# Load the data
-dataset, dataloader = load_data(path=TRAINPATH, batch_size=32, shuffle=shuffle)
+    # Load the data
+    dataset, dataloader = load_data(path=TRAINPATH, batch_size=32, shuffle=shuffle)
 
-# Visualize random data points with their labels
-plot_data(dataloader, n_images=4)
+    # Visualize random data points with their labels
+    plot_data(dataloader, n_images=4)
 
-# Do the pose estimation
-mp_pose = mp.solutions.pose  # available poses
-estimated_poses, annotated_images = estimate_poses(dataloader)
+    # Do the pose estimation
+    mp_pose = mp.solutions.pose  # available poses
+    estimated_poses, annotated_images = estimate_poses(dataloader)
 
-plot_image_grid(annotated_images, n_images=4)
-dataloader
+    plot_image_grid(annotated_images, n_images=4)
