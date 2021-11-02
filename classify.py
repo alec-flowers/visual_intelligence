4f940171c4cb6f5236399e3ee76763760fcf44df
Nina-Mainusch <Nina.Mainusch@protonmail.com>
2021-11-02 12:36:53 +0100

    REFACTOR: put pose estimation in pose.py, classifier in classiy.py

--- classify.py
+++ classify.py
@@ -13,9 +13,6 @@ from pytorch_lightning.loggers import TensorBoardLogger
 if __name__ == "__main__":
     # Define parameters
     shuffle = True
-    run_from_scratch = False
-    subset = False  # Take a subset of 100 images out of the 660 images?
-    save_poses = False  # Save poses after estimated?
 
     # Model parameters
     batch_size = 32
@@ -23,66 +20,26 @@ if __name__ == "__main__":
     gpus = 0
 
     # Load the data
-    dataset, dataloader = load_data(path=TRAINPATH, batch_size=None, shuffle=shuffle, subset=subset)
+    numpy_data_world = load_pickle(PICKLEDPATH, "pose_world_landmark_numpy.pickle")
+    labels_drop_na = load_pickle(PICKLEDPATH, "labels_drop_na.pickle")
+    train_coordinate_dataset = CoordinatesDataset(numpy_data_world, labels_drop_na, set_type="train")
+    val_coordinate_dataset = CoordinatesDataset(numpy_data_world, labels_drop_na, set_type="val")
+    train_dataloader = DataLoader(train_coordinate_dataset, batch_size=batch_size, shuffle=True, num_workers=12)
+    val_dataloader = DataLoader(val_coordinate_dataset, batch_size=batch_size, num_workers=12)
 
-    # TODO: I suspect the annotated images take lots of memory, for me (Nina) there was an issue when loading them
-    #  from the pickle
-    if run_from_scratch:
-        # Do the pose estimation
-        estimated_poses, annotated_images = poses_for_dataset(dataloader)
+    # Init the model
+    pl.seed_everything(42)
+    mlp = MLP(num_classes=3)
+    logger = TensorBoardLogger("tb_logs", name="mlp")
+    # Configure the training
+    trainer = pl.Trainer(gpus=gpus, deterministic=True, log_every_n_steps=5, max_epochs=epochs, logger=logger)
+    # Start training
+    trainer.fit(mlp, train_dataloader)
 
-        # NOTE NUMPY DATA TAKES OUT NULLS, will have to take out nulls in labels
-        df, df_vis, numpy_data, labels_drop_na = pose_to_dataframe(estimated_poses, dataset, pose_var='pose_landmarks')
-        df_world, df_vis_world, numpy_data_world, _ = pose_to_dataframe(estimated_poses, dataset,
-                                                                        pose_var='pose_world_landmarks')
-        if save_poses:
-            save_dataframes_to_pickle(PICKLEDPATH,
-                                      [df, df_vis, numpy_data, df_world, df_vis_world, numpy_data_world,
-                                       labels_drop_na, annotated_images],
-                                      POSEDATAFRAME_LIST
-                                      )
+    # TODO how to include validation
+    #trainer.fit(mlp, train_dataloader, val_dataloader)
+    mlp
+    # TODO visualize coordinates together with labels to verify they are aligned
 
-    else:
-        # df = load_pickle(PICKLEDPATH, "pose_landmark_all_df.pickle")
-        # df_vis = load_pickle(PICKLEDPATH, "pose_landmark_vis_df.pickle")
-        # numpy_data = load_pickle(PICKLEDPATH, "pose_landmark_numpy.pickle")
-        # df_world = load_pickle(PICKLEDPATH, "pose_world_landmark_all_df.pickle")
-        # df_vis_world = load_pickle(PICKLEDPATH, "pose_world_landmark_vis_df.pickle")
-
-        # annotated_images = load_pickle(PICKLEDPATH, "annotated_images.pickle") # TODO doesnt work for me (Nina)
-
-        # plot_dataset_images(dataset, 9)
-        # plot_annotated_images(annotated_images, 16)  # TODO doesnt work
-        # plot_no_pose_photo(df, dataset, 9)
-
-        # print(f"There are {sum(df['NOSE'].isna())} images we don't get a pose estimate for out \
-        # of {len(dataset)}. This is {sum(df['NOSE'].isna()) / len(dataset) * 100:.2f}%")
-
-        # df_vis = df_vis.dropna(axis=0, how='any')
-        # df_vis.describe()
-        # df_vis.mean().sort_values(ascending=False)
-
-        # Load the data
-        numpy_data_world = load_pickle(PICKLEDPATH, "pose_world_landmark_numpy.pickle")
-        labels_drop_na = load_pickle(PICKLEDPATH, "labels_drop_na.pickle")
-        train_coordinate_dataset = CoordinatesDataset(numpy_data_world, labels_drop_na, set_type="train")
-        val_coordinate_dataset = CoordinatesDataset(numpy_data_world, labels_drop_na, set_type="val")
-        train_dataloader = DataLoader(train_coordinate_dataset, batch_size=batch_size, shuffle=True, num_workers=12)
-        val_dataloader = DataLoader(val_coordinate_dataset, batch_size=batch_size, num_workers=12)
-
-        # Init the model
-        pl.seed_everything(42)
-        mlp = MLP(num_classes=3)
-        logger = TensorBoardLogger("tb_logs", name="mlp")
-        # Configure the training
-        trainer = pl.Trainer(gpus=gpus, deterministic=True, log_every_n_steps=5, max_epochs=epochs, logger=logger)
-        # Start training
-        trainer.fit(mlp, train_dataloader)
-
-        # TODO how to include validation
-        #trainer.fit(mlp, train_dataloader, val_dataloader)
-        mlp
-        # TODO visualize coordinates together with labels to verify they are aligned
-
-        # INSPECT training
-        # run in terminal: tensorboard --logdir=tb_logs
+    # INSPECT training
+    # run in terminal: tensorboard --logdir=tb_logs
--- pose.py
+++ pose.py
@@ -1,8 +1,23 @@
+import os
+import sys
+
 import cv2
 import mediapipe as mp
-import numpy as np
-from plot import *
+import pandas as pd
 from tqdm import tqdm
+
+from data import load_data
+from plot import *
+from utils import LANDMARK_DICT, load_pickle
+from utils import TRAINPATH, PICKLEDPATH, save_dataframes_to_pickle, \
+    POSEDATAFRAME_LIST
+
+# from plot import *
+
+
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -11,7 +26,6 @@ mp_pose = mp.solutions.pose
 
 def poses_for_dataset(dataloader):
     assert dataloader.batch_size is None
-
     result_list = []
     annotated_images = []
     for image, label in tqdm(dataloader):
@@ -64,3 +78,91 @@ def estimate_poses(image, label, plot=False):
             # mp_drawing.plot_landmarks(
             #    results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
     return results, annotated_image
+
+
+def pose_to_dataframe(estimated_poses, dataset, pose_var):
+    all_val = []
+    visib_val = []
+    for_numpy = []
+    for i in range(len(estimated_poses)):
+        if estimated_poses[i]:
+            val = []
+            visib = []
+            nump = []
+            for land in getattr(estimated_poses[i], pose_var).landmark:
+                x = land.x
+                y = land.y
+                z = land.z
+                vis = land.visibility
+                val.append([x, y, z, vis])
+                nump.append([x, y, z])
+                visib.append(vis)
+            all_val.append(val)
+            visib_val.append(visib)
+            for_numpy.append(nump)
+        else:
+            all_val.append([None])
+            visib_val.append([None])
+
+    df = pd.DataFrame.from_records(all_val)
+    df_vis = pd.DataFrame.from_records(visib_val)
+
+    df = df.rename(LANDMARK_DICT, axis=1)
+    df_vis = df_vis.rename(LANDMARK_DICT, axis=1)
+
+    labels = []
+    for _, label in dataset:
+        labels.append(label)
+    df['label'] = labels
+    df_vis['label'] = labels
+
+    labels_drop_na = np.array(df_vis.dropna(axis=0, how='any')['label'])
+
+    return df, df_vis, np.array(for_numpy), labels_drop_na
+
+
+if __name__ == "__main__":
+    # Define parameters
+    shuffle = True
+    run_from_scratch = False
+    subset = False  # Take a subset of 100 images out of the 660 images?
+    save_poses = False  # Save poses after estimated?
+
+    # Load the data
+    dataset, dataloader = load_data(path=TRAINPATH, batch_size=None, shuffle=shuffle, subset=subset)
+
+    # TODO: I suspect the annotated images take lots of memory, for me (Nina) there was an issue when loading them
+    #  from the pickle
+    if run_from_scratch:
+        # Do the pose estimation
+        estimated_poses, annotated_images = poses_for_dataset(dataloader)
+
+        # NOTE NUMPY DATA TAKES OUT NULLS, will have to take out nulls in labels
+        df, df_vis, numpy_data, labels_drop_na = pose_to_dataframe(estimated_poses, dataset, pose_var='pose_landmarks')
+        df_world, df_vis_world, numpy_data_world, _ = pose_to_dataframe(estimated_poses, dataset,
+                                                                        pose_var='pose_world_landmarks')
+        if save_poses:
+            save_dataframes_to_pickle(PICKLEDPATH,
+                                      [df, df_vis, numpy_data, df_world, df_vis_world, numpy_data_world,
+                                       labels_drop_na, annotated_images],
+                                      POSEDATAFRAME_LIST
+                                      )
+    else:
+        df = load_pickle(PICKLEDPATH, "pose_landmark_all_df.pickle")
+        df_vis = load_pickle(PICKLEDPATH, "pose_landmark_vis_df.pickle")
+        numpy_data = load_pickle(PICKLEDPATH, "pose_landmark_numpy.pickle")
+        df_world = load_pickle(PICKLEDPATH, "pose_world_landmark_all_df.pickle")
+        df_vis_world = load_pickle(PICKLEDPATH, "pose_world_landmark_vis_df.pickle")
+
+        # annotated_images = load_pickle(PICKLEDPATH, "annotated_images.pickle") # TODO doesnt work for me (Nina)
+
+        plot_dataset_images(dataset, 9)
+        # plot_annotated_images(annotated_images, 16)  # TODO doesnt work
+        plot_no_pose_photo(df, dataset, 9)
+
+        print(f"There are {sum(df['NOSE'].isna())} images we don't get a pose estimate for out \
+        of {len(dataset)}. This is {sum(df['NOSE'].isna()) / len(dataset) * 100:.2f}%")
+
+        df_vis = df_vis.dropna(axis=0, how='any')
+        df_vis.describe()
+        df_vis.mean().sort_values(ascending=False)
