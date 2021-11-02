b7c2300f7f03b803b5360f772c4d52b8a770b205
Nina-Mainusch <Nina.Mainusch@protonmail.com>
2021-11-02 16:00:58 +0100

    FEAT: add CNN architecture

--- model.py
+++ model.py
@@ -8,6 +8,7 @@ class MLP(pl.LightningModule):
     """
     Multilayer Perceptron.
     """
+
     def __init__(self, num_classes):
         super().__init__()
         self.layers = nn.Sequential(
@@ -43,6 +44,7 @@ class MLP(pl.LightningModule):
         self.log("val_loss", loss)
         # log step metric
         self.log('val_acc', self.valid_acc(y_hat, y), on_epoch=True, prog_bar=True, logger=True)
+        return loss
 
     def training_epoch_end(self, outs):
         # log epoch metric
@@ -53,3 +55,47 @@ class MLP(pl.LightningModule):
         return optimizer
 
 
+class CNN(pl.LightningModule):
+    """
+    CNN. Skip the pose estimation, classify on the raw image.
+    """
+
+    def __init__(self, num_classes):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=16, kernel_size=3),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(16, num_classes)
+        )
+        self.ce = nn.CrossEntropyLoss()
+        self.train_acc = torchmetrics.Accuracy()
+        self.valid_acc = torchmetrics.Accuracy()
+
+    def forward(self, x):
+        return self.layers(x)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        x = x.view(x.size(0), -1).float()
+        y_hat = self.layers(x)
+        loss = self.ce(y_hat, y)
+        self.log('train_loss', loss)
+        self.log('train_acc', self.train_acc(y_hat, y), on_epoch=True, prog_bar=True, logger=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        x = x.view(x.size(0), -1).float()
+        y_hat = self.layers(x)
+        loss = self.ce(y_hat, y)
+        self.log("val_loss", loss)
+        self.log('val_acc', self.valid_acc(y_hat, y), on_epoch=True, prog_bar=True, logger=True)
+        return loss
+
+    def training_epoch_end(self, outs):
+        self.log('train_acc_epoch', self.train_acc.compute())
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
+        return optimizer
