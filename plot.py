b4743eb9b81f4b8d47583e3f84113e4ca73b2af7
Nina-Mainusch <Nina.Mainusch@protonmail.com>
2021-11-11 11:17:45 +0100

    FEAT: plot misclassified images MLP

--- plot.py
+++ plot.py
@@ -30,13 +30,16 @@ def plot_image(image, dataloader=False, label=None, title=''):
     plt.show()
 
 
-def plot_image_grid(images, n_images, dataloader=False, title=""):
+def plot_image_grid(images, n_images, dataloader=False, title="", subplot_title=[]):
     for i in range(n_images):
         if dataloader:
             image = images[i].numpy().transpose((1, 2, 0))
         else:
             image = images[i]
-        plt.subplot(math.ceil(np.sqrt(n_images)), math.ceil(np.sqrt(n_images)), i + 1)
+        if subplot_title:
+            plt.subplot(math.ceil(np.sqrt(n_images)), math.ceil(np.sqrt(n_images)), i + 1).set_title(f"t: {subplot_title[i][0]}, p: {subplot_title[i][1]}")
+        else:
+            plt.subplot(math.ceil(np.sqrt(n_images)), math.ceil(np.sqrt(n_images)), i + 1)
         plt.xticks([])
         plt.yticks([])
         plt.grid(False)
@@ -92,3 +95,16 @@ def plot_no_pose_photo(df, dataset, num=9):
         i += 1
 
     plot_image_grid(bad_photos, len(bad_photos), dataloader=True, title="Photos where no pose was detected")
+
+
+def plot_misclassfied_images(targets, predictions, all_images, type="training", max_n_to_plot=16):
+    # Extract images that are misspredicted
+    errors = targets - predictions
+    error_indices = np.where(errors != 0)
+    misclassified_images = [all_images[i] for i in error_indices[0]]
+    # Create subplot titles, consisting of targets and predictions
+    subplot_title = list(zip([targets[i].item() for i in error_indices[0]],
+                             [predictions[i].item() for i in error_indices[0]]))
+    plot_n_images = min(len(misclassified_images), max_n_to_plot)
+    plot_image_grid(misclassified_images[:plot_n_images], plot_n_images, dataloader=False,
+                    title=f"Misclassified {type} images", subplot_title=subplot_title)
