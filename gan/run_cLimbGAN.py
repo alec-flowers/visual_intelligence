54dd604402335fceb3bc697c86c7dc6744221902
Nina-Mainusch <Nina.Mainusch@protonmail.com>
2021-12-15 09:43:18 +0100

    Chore: docstrings

--- gan/run_cLimbGAN.py
+++ gan/run_cLimbGAN.py
@@ -9,12 +9,38 @@ from pose.plot import plot_3d_keypoints
 from pose.utils import GOOD_POSES_PATH, NOISE_DIMENSION, calc_limb_lengths, PLOT_PATH, CLIMBGAN_PATH
 
 
-def differences_in_limb_lengths(generated_limb_length, original_limb_length):
+def differences_in_limb_lengths(generated_limb_length: np.array, original_limb_length: np.array) -> float:
+    """
+    Calculate the differences in limb lengths from the generated to the original image to check
+    if GAN converges.
+    :param generated_limb_length:
+    :type generated_limb_length:
+    :param original_limb_length:
+    :type original_limb_length:
+    :return: the differences
+    :rtype: float
+    """
     difference = np.linalg.norm(generated_limb_length - original_limb_length)
     return difference
 
 
-def generate_coords_given_limb_lengths(limb_lengths, label, version, plot=False):
+def generate_coords_given_limb_lengths(limb_lengths: np.array,
+                                       label: int,
+                                       version: int,
+                                       plot: bool = False) -> np.array:
+    """
+    Generate the coordinates of a pose given the label and the limb lengths of a person.
+    :param limb_lengths:  the person's limb lengths
+    :type limb_lengths: np.array
+    :param label: the desired pose label
+    :type label: int
+    :param version: the version of the GAN to consult
+    :type version: int
+    :param plot: whether to plot the generated image
+    :type plot: bool
+    :return: the generated limb lengths
+    :rtype: np.array
+    """
     device = get_device()
     generator = LimbLengthGenerator().to(device)
     generator = load_generator(generator, version, CLIMBGAN_PATH)
@@ -30,10 +56,10 @@ def generate_coords_given_limb_lengths(limb_lengths, label, version, plot=False)
         coord = generated_coords.detach().numpy().squeeze(0)
         plot_3d_keypoints(coord[:, 0], coord[:, 1], coord[:, 2], elev=-70, azim=270)
     else:
-        return generated_limb_lengths
+        return generated_limb_lengths[0]
 
 
-def plot_limb_length_convergence(mean_differences):
+def plot_limb_length_convergence(mean_differences: list):
     plt.plot(*zip(*mean_differences))
     plt.title("Mean squared average deviation of generated from target limb lengths")
     plt.xlabel("Training epoch")
@@ -45,7 +71,6 @@ def plot_limb_length_convergence(mean_differences):
 
 if __name__ == "__main__":
     VERSION = 780
-    device = get_device()
     train_loader, _, train_coordinate_dataset, _ = get_data(batch_size=64, split_ratio=0.15, path=GOOD_POSES_PATH)
 
     # Plot generated images conditioned on label and limb length
@@ -63,7 +88,7 @@ if __name__ == "__main__":
                                                train_coordinate_dataset.labels[0],
                                                version=version,
                                                plot=False)
-        differences = differences_in_limb_lengths(generated_limb_length[0], train_coordinate_dataset.limb_lengths[0])
+        differences = differences_in_limb_lengths(generated_limb_length, train_coordinate_dataset.limb_lengths[0])
         mean_differences.append((version, differences))
 
     plot_limb_length_convergence(mean_differences)
